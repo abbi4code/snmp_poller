57ccd35796984d666b6c5b9a0876bf3ad57afac8
Abhishek Raj <abhishek4code@gmail.com>
2025-05-22 00:13:52 +0530

    chore: added sequencial steps to poll all the devices concurrently

---
+++ poller/main.py
@@ -0,0 +1,4 @@
+# soo here first we create a instance for each hostname okay
+# then we get the polled the polled data from each hostname that we can print
+# also here we will use asyncio.gather again to polled devices concurrently
+# we need to first parse the config file 
